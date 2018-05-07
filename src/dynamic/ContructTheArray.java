fd4d1c5fb64b018583f2840fcc6b83c0cc41f3dd
pjimenez <pjimenez@homeaway.com>
2018-05-07 18:04:19 +0200

    add construct the array problem  Changes to be committed: 	new file:   src/dynamic/ContructTheArray.java

---
+++ src/dynamic/ContructTheArray.java
@@ -0,0 +1,31 @@
+package dynamic;
+
+
+import java.util.Scanner;
+
+public class ContructTheArray {
+
+    private static long countArray(int length, int max, int lastElement) {
+
+        final int mod = 1000000007;
+        final long[] previousState = {1,0};
+        final long[] state = {1,0};
+        for (int i = 1; i < length; i++){
+            state[0] = ((max - 1) * previousState[1]) % mod;
+            state[1] = ((max - 2) * previousState[1] + previousState[0]) % mod;
+            previousState[0] = state[0];
+            previousState[1] = state[1];
+        }
+        return lastElement == 1 ? state[0] : state[1];
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int x = in.nextInt();
+        long answer = countArray(n, k, x);
+        System.out.println(answer);
+        in.close();
+    }
+}
